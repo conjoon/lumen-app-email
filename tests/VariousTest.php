253182e088836f04d8f71a3d3dce58fe92717551
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2019-07-06 18:44:53 +0200

    - enhancement: minor refactorings, added tests

--- tests/VariousTest.php
+++ tests/VariousTest.php
@@ -24,26 +24,21 @@
  * USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 
+class VariousTest extends TestCase
+{
+    /**
+     * Get information and validate registered middleware for the app.
+     * 
+     * @return void
+     */
+    public function testMiddleware()
+    {
+        $reflection = new \ReflectionClass($this->app);
+        $property = $reflection->getMethod('gatherMiddlewareClassNames');
+        $property->setAccessible(true);
+        $ret = $property->invokeArgs($this->app, ['auth']);
 
-$router->post('cn_imapuser/auth', 'UserController@authenticate');
-
-
-$router->get('cn_mail/MailAccounts', ['middleware' => 'auth', function () use ($router) {
-    return $router->app->version();
-}]);
-
-/*
-$router->group(['prefix' => 'api/'], function ($app) {
-    $app->get('cn_mail/MailAccounts','MailAccountController@get');
-});*/
+        $this->assertSame($ret[0], "App\Http\Middleware\Authenticate");
+    }
+}
---
+++ tests/app/Http/Middleware/AuthenticateTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * conjoon
+ * php-cn_imapuser
+ * Copyright (C) 2019 Thorsten Suckow-Homberg https://github.com/conjoon/php-cn_imapuser
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+use App\Http\Middleware\Authenticate;
+
+class AuthenticateTest extends TestCase
+{
+    /**
+     * Tests handle() to make sure either 401 is called or next is chained.
+     *
+     * @return void
+     */
+    public function testHandle()
+    {
+        $authStub = $this->getMockBuilder('\Illuminate\Auth\AuthManager')
+                     ->disableOriginalConstructor()
+                     ->getMock();
+
+        $stubbedStub = new class {
+
+            public static $ISGUEST = true;
+
+            public function guest() {
+                return self::$ISGUEST;
+            }
+        };
+
+        $authStub->method('guard')
+                 ->willReturn($stubbedStub);
+
+        $authenticate = new Authenticate($authStub);
+
+        // test for is guest
+        $stubbedStub::$ISGUEST = true;
+        $response = $authenticate->handle(new \Illuminate\Http\Request(), function(){});
+        $this->assertInstanceOf(\Illuminate\Http\Response::class, $response);
+        $this->assertSame($response->getStatusCode(), 401);
+
+        // test for authenticated
+        $stubbedStub::$ISGUEST = false;
+        $newRequest = new \Illuminate\Http\Request();
+        $cmpRequest = null;
+        $called = false;
+        $response = $authenticate->handle($newRequest, function ($request) use ($newRequest, &$called){
+            $this->assertSame($newRequest, $request);
+            $called = true;
+        });
+        $this->assertNull($response);
+        $this->assertTrue($called);
+
+
+
+    }
+}
