82de69b3ae3f828dc1431518757b695fb76c8c08
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2019-07-27 10:13:32 +0200

    - enhancement: added PreviewableMessageItem for specifying previewTexts for MessageItems

---
+++ app/Conjoon/Mail/Client/Data/PreviewableMessageItem.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * conjoon
+ * php-cn_imapuser
+ * Copyright (C) 2019 Thorsten Suckow-Homberg https://github.com/conjoon/php-cn_imapuser
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+declare(strict_types=1);
+
+namespace Conjoon\Mail\Client\Data;
+
+
+/**
+ * Class PreviewableMessageItem is a MessageItem containing body text for previewing the
+ * message's contents.
+ *
+ * @example
+ *
+ *    $item = new PreviewableMessageItem(
+ *              new MessageKey("INBOX", "232"),
+ *              ["previewText" => "foobar"]
+ *            );
+ *
+ *    $item->getPreviewText; // "foobar"
+ *
+ * @package Conjoon\Mail\Client\Data
+ */
+class PreviewableMessageItem extends MessageItem  {
+
+
+    /**
+     * @var string
+     */
+    protected $previewText = "";
+
+
+    /**
+     * Sets the previewText for this MessageItem. The text should be UTF-8
+     * encoded and stripped from all HTML tags.
+     *
+     * @param string $text
+     */
+    public function setPreviewText(string $text) {
+        $this->previewText = $text;
+    }
+
+
+    /**
+     * Returns the previewText for this MessageItem.
+     * @return string
+     */
+    public function getPreviewText() {
+        return $this->previewText;
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function toArray() :array {
+
+        $arr = parent::toArray();
+
+        $arr["previewText"] = $this->getPreviewText();
+
+        return $arr;
+    }
+
+}
---
+++ tests/app/Conjoon/Mail/Client/Data/PreviewableMessageItemTest.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * conjoon
+ * php-cn_imapuser
+ * Copyright (C) 2019 Thorsten Suckow-Homberg https://github.com/conjoon/php-cn_imapuser
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+use Conjoon\Mail\Client\Data\MessageItem,
+    Conjoon\Mail\Client\Data\PreviewableMessageItem,
+    Conjoon\Mail\Client\Data\MessageKey;
+
+
+class PreviewableMessageItemTest extends TestCase
+{
+
+
+// ---------------------
+//    Tests
+// ---------------------
+
+    /**
+     * Tests constructor
+     */
+    public function testConstructor() {
+
+        $messageKey = $this->createMessageKey();
+        $messageItem = new PreviewableMessageItem($messageKey);
+        $this->assertInstanceOf(MessageItem::class, $messageItem);
+    }
+
+
+    /**
+     * Test class.
+     */
+    public function testClass() {
+
+        $previewText = "foobar";
+
+        $messageKey = $this->createMessageKey();
+
+        $messageItem = new PreviewableMessageItem(
+            $messageKey, ["subject" => "YO!", "previewText" => $previewText]
+        );
+
+        $this->assertSame($previewText, $messageItem->getPreviewText());
+
+        $messageItem->setPreviewText("snafu");
+        $this->assertSame("snafu", $messageItem->getPreviewText());
+
+        $arr = $messageItem->toArray();
+        $this->assertArraySubset([
+            "subject" => "YO!",
+            "previewText" => "snafu"
+        ], $arr);
+
+    }
+
+
+
+// ---------------------
+//    Helper Functions
+// ---------------------
+    /**
+     * Returns a MessageKey.
+     *
+     * @param string $mailFolderId
+     * @param string $id
+     *
+     * @return MessageKey
+     */
+    protected function createMessageKey($mailFolderId = "INBOX", $id = "232") :MessageKey {
+        return new MessageKey($mailFolderId, $id);
+    }
+
+}
