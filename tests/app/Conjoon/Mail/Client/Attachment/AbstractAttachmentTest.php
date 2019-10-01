cc326283d208c988c14a51311c2f3f97ec263826
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2019-10-01 15:31:55 +0200

    untracked files on master: 428ea9a Merge branch 'dev'

---
+++ tests/app/Conjoon/Mail/Client/Attachment/AbstractAttachmentTest.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * conjoon
+ * php-cn_imapuser
+ * Copyright (C) 2019 Thorsten Suckow-Homberg https://github.com/conjoon/php-cn_imapuser
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+use Conjoon\Mail\Client\Data\CompoundKey\AttachmentKey,
+    Conjoon\Util\Jsonable,
+    Conjoon\Mail\Client\Attachment\AbstractAttachment;
+
+
+class AbstractAttachmentTest extends TestCase
+{
+
+
+// ---------------------
+//    Tests
+// ---------------------
+
+    /**
+     * Tests constructor
+     */
+    public function testClass() {
+
+        $type          = "image/jpg";
+        $text          = "Text";
+        $size          = 123;
+
+        $attachment = $this->createAbstractAttachment(
+            new AttachmentKey("dev", "INBOX", "123", "1"),
+            ["type" => $type,
+            "text" => $text,
+            "size" => $size]
+        );
+
+        $this->assertSame($type, $attachment->getType());
+        $this->assertSame($text, $attachment->getText());
+        $this->assertSame($size, $attachment->getSize());
+
+    }
+
+
+    /**
+     * Tests constructor with exception for missing type
+     */
+    public function testConstructor_exceptionType() {
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("type");
+
+        $this->createAbstractAttachment(
+            new AttachmentKey("dev", "INBOX", "123", "1"),
+            [//type" => "1",
+             "text" => "2",
+             "size" => 3,]
+        );
+    }
+
+
+    /**
+     * Tests constructor with exception for missing text
+     */
+    public function testConstructor_exceptionText() {
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("text");
+
+        $this->createAbstractAttachment(
+            new AttachmentKey("dev", "INBOX", "123", "1"),
+            ["type" => "1",
+             //   "text" => "2",
+                "size" => 3]
+        );
+    }
+
+
+    /**
+     * Tests constructor with exception for missing size
+     */
+    public function testConstructor_exceptionSize() {
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("size");
+
+        $this->createAbstractAttachment(
+            new AttachmentKey("dev", "INBOX", "123", "1"),
+            ["type" => "1",
+                   "text" => "2"
+                //"size" => 3
+            ]
+
+        );
+    }
+
+
+
+// ---------------------
+//    Helper Functions
+// ---------------------
+
+    /**
+     * @return AbstractAttachment
+     */
+    protected function createAbstractAttachment($key, $data) :AbstractAttachment {
+        // Create a new instance from the Abstract Class
+        return new class($key, $data) extends AbstractAttachment {
+
+        };
+    }
+
+
+}
