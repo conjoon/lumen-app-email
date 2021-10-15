0c491a4574c1c255c355ac2561746c9c0b5e7d55
ThorstenSuckow <thorsten@suckow-homberg.de>
2021-10-16 01:27:02 +0200

    refactor: apply project coding standards

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * conjoon
  * php-ms-imapuser
@@ -27,9 +28,11 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
@@ -52,23 +55,28 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param Exception $e
+     *
      * @return void
+     *
+     * @throws Exception
      */
-    public function report(Exception $exception)
+    public function report(Exception $e)
     {
-        parent::report($exception);
+        parent::report($e);
     }
+
 
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     * @param Request $request
+     * @param Exception $e
+     *
+     * @return Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Exception $e): Response
     {
-        return parent::render($request, $exception);
+        return parent::render($request, $e);
     }
 }
