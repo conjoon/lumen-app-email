1ac5873fd9cbee5104b0f2ceba43e9df0644c6b7
ThorstenSuckow <thorsten@suckow-homberg.de>
2022-09-08 14:19:50 +0200

    refactor: add validation class drafts
    refs conjoon/php-lib-conjoon#8

---
+++ lib/tests/Http/V0/JsonApi/Query/Validation/MailFolderValidatorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * conjoon
+ * lumen-app-email
+ * Copyright (c) 2022 Thorsten Suckow-Homberg https://github.com/conjoon/lumen-app-email
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\App\Http\V0\Query\Validation;
+
+use App\Http\V0\JsonApi\Query\Validation\MailFolderValidator;
+use Conjoon\JsonApi\Query\Validation\Validator;
+use Tests\TestCase;
+
+/**
+ *
+ */
+class MailFolderValidatorTest extends TestCase
+{
+    /**
+     * test class
+     */
+    public function testClass()
+    {
+        $inst = new MailFolderValidator();
+        $this->assertInstanceOf(Validator::class, $inst);
+    }
+}
