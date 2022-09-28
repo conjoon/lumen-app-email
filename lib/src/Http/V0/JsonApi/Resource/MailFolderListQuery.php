9503e0bf1c1264c0b148a5954241616635f8e498
ThorstenSuckow <thorsten@suckow-homberg.de>
2022-09-28 17:45:11 +0200

    refactor: add MailFolderListQuery
    refs conjoon/php-lib-conjoon#8

---
+++ lib/src/Http/V0/JsonApi/Resource/MailFolderListQuery.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * conjoon
+ * lumen-app-email
+ * Copyright (C) 2021-2022 Thorsten Suckow-Homberg https://github.com/conjoon/lumen-app-email
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace App\Http\V0\JsonApi\Resource;
+
+use Conjoon\Mail\Client\Data\Resource\MailFolderListQuery as BaseMailFolderListQuery;
+
+/**
+ * Class MailFolderListQuery.
+ */
+class MailFolderListQuery extends BaseMailFolderListQuery
+{
+    /**
+     * Returns the fields that should be queried. If no fields where specified, this implementation
+     * will return the default fields of the resource target for this query.
+     *
+     * @return array
+     */
+    public function getFields(): array
+    {
+        $defaultFields = $this->getResourceTarget()->getDefaultFields();
+
+        $relfields = $this->{"relfield:fields[MailFolder]"};
+        $fields    = $this->{"fields[MailFolder]"};
+
+        if (!$relfields) {
+            return $fields ? explode(",", $fields) : $defaultFields;
+        }
+
+        $relfields = explode(",", $relfields);
+
+        foreach ($relfields as $relfield) {
+            $prefix    = substr($relfield, 0, 1);
+            $fieldName = substr($relfield, 1);
+
+            if ($prefix === "-") {
+                $defaultFields = array_filter($defaultFields, fn ($field) => $field !== $fieldName);
+            } else {
+                if (!in_array($fieldName, $defaultFields)) {
+                    $defaultFields[] = $fieldName;
+                }
+            }
+        }
+
+        return $defaultFields;
+    }
+
+
+
+    /**
+     * Returns the resource target of this query.
+     *
+     * @return MailFolder
+     */
+    function getResourceTarget(): MailFolder
+    {
+        return new MailFolder();
+    }
+}
---
+++ lib/tests/Http/V0/JsonApi/Resource/MailFolderListQueryTest.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * conjoon
+ * lumen-app-email
+ * Copyright (c) 2019-2022 Thorsten Suckow-Homberg https://github.com/conjoon/lumen-app-email
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\App\Http\V0\JsonApi\Resource;
+
+use Conjoon\Core\Data\ParameterBag;
+use App\Http\V0\JsonApi\Resource\MailFolderListQuery;
+use App\Http\V0\JsonApi\Resource\MailFolder;
+use Conjoon\Mail\Client\Data\Resource\MailFolderListQuery as BaseMailFolderListQuery;
+use Tests\TestCase;
+
+/**
+ * Tests MailFolderListQuery.
+ */
+class MailFolderListQueryTest extends TestCase
+{
+    /**
+     * test class
+     */
+    public function testClass()
+    {
+        $inst = new MailFolderListQuery(new ParameterBag([]));
+        $this->assertInstanceOf(BaseMailFolderListQuery::class, $inst);
+    }
+
+
+    /**
+     * Tests getResourceTarget()
+     * @return void
+     */
+    public function testGetResourceTarget(): void
+    {
+        $parameterBag = new ParameterBag();
+        $query = new MailFolderListQuery($parameterBag);
+        $this->assertSame(MailFolder::class, get_class($query->getResourceTarget()));
+    }
+
+
+    /**
+     * Tests getFields()
+     * @return void
+     */
+    public function testGetFields(): void
+    {
+        // fields[MailFolder]=name,data,folderType
+        $parameterBag = new ParameterBag();
+        $resourceTarget = $this->createMockForAbstract(MailFolder::class, ["getDefaultFields"]);
+        $resourceTarget->expects($this->once())->method("getDefaultFields")->willReturn(
+            ["name", "data", "folderType"]
+        );
+        $query = $this->createMockForAbstract(
+            MailFolderListQuery::class,
+            ["getResourceTarget"],
+            [$parameterBag]
+        );
+        $query->expects($this->once())->method("getResourceTarget")->willReturn($resourceTarget);
+        $this->assertSame(["name", "data", "folderType"], $query->getFields());
+
+        // fields[MailFolder]=name,data,folderType
+        $parameterBag = new ParameterBag(["fields[MailFolder]" => "name,data,folderType"]);
+        $query = new MailFolderListQuery($parameterBag);
+        $this->assertEqualsCanonicalizing(["name", "data", "folderType"], $query->getFields());
+
+        // relfield:fields[MailFolder]=+unreadMessages,-data,-folderType
+        $parameterBag = new ParameterBag(["relfield:fields[MailFolder]" => "+unreadMessages,-data,-folderType"]);
+        $resourceTarget = $this->createMockForAbstract(MailFolder::class, ["getDefaultFields"]);
+        $resourceTarget->expects($this->once())->method("getDefaultFields")->willReturn(
+            ["name", "data", "folderType"]
+        );
+        $query = $this->createMockForAbstract(
+            MailFolderListQuery::class,
+            ["getResourceTarget"],
+            [$parameterBag]
+        );
+        $query->expects($this->once())->method("getResourceTarget")->willReturn($resourceTarget);
+        $this->assertEqualsCanonicalizing(["name", "unreadMessages"], $query->getFields());
+    }
+}
