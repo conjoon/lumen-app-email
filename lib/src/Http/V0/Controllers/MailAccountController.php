6cb9b62d7c0fc8152d5ae0752abf709dfb227d16
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2024-02-05 15:27:13 +0100

    refactor: consider MailAccountList instead of array of MailAccounts

--- lib/src/Http/V0/Controllers/MailAccountController.php
+++ lib/src/Http/V0/Controllers/MailAccountController.php
@@ -1,28 +1,12 @@
 <?php
 
 /**
- * conjoon
- * lumen-app-email
- * Copyright (c) 2019-2022 Thorsten Suckow-Homberg https://github.com/conjoon/lumen-app-email
+ * This file is part of the conjoon/php-lib-conjoon project.
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * (c) 2019-2024 Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * For full copyright and license information, please consult the LICENSE-file distributed
+ * with this source code.
  */
 
 declare(strict_types=1);
@@ -50,14 +34,10 @@ class MailAccountController extends Controller
         $user = Auth::user();
 
         $accounts = $user->getMailAccounts();
-        $res = [];
-        foreach ($accounts as $acc) {
-            $res[] = $acc->toArray();
-        }
 
         return response()->json([
             "success" => true,
-            "data" => $res
+            "data" => $accounts->toArray()
         ]);
     }
 }
--- lib/tests/TestTrait.php
+++ lib/tests/TestTrait.php
@@ -1,28 +1,12 @@
 <?php
 
 /**
- * conjoon
- * lumen-app-email
- * Copyright (c) 2019-2022 Thorsten Suckow-Homberg https://github.com/conjoon/lumen-app-email
+ * This file is part of the conjoon/php-lib-conjoon project.
  *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * (c) 2019-2024 Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
  *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * For full copyright and license information, please consult the LICENSE-file distributed
+ * with this source code.
  */
 
 declare(strict_types=1);
@@ -30,6 +14,7 @@ declare(strict_types=1);
 namespace Tests;
 
 use Conjoon\Mail\Client\Data\MailAccount;
+use Conjoon\MailClient\Data\MailAccountList;
 use Illuminate\Contracts\Auth\Authenticatable;
 use PHPUnit\Framework\MockObject\MockObject;
 use ReflectionClass;
@@ -85,7 +70,7 @@ trait TestTrait
                  }));
 
         $userStub->method("getMailAccounts")
-                 ->willReturn([$this->getTestMailAccount("dev_sys_conjoon_org")]);
+                 ->willReturn(MailAccountList::make($this->getTestMailAccount("dev_sys_conjoon_org")));
 
         return $userStub;
     }
